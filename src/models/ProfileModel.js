ec1775d87b6461d77f9e52121b3f006ea3006386
rahyanakil <rahyanakil89@gmail.com>
2025-04-21 00:58:02 +0600

    latest error update

--- src/models/ProfileModel.js
+++ src/models/ProfileModel.js
@@ -4,7 +4,7 @@ const DataSchema = mongoose.Schema(
     FirstName: { type: String },
     LastName: { type: String },
     EmailAddress: { type: String },
-    MobileNumber: { type: String },
+    MobileNumber: { type: String, unique: true },
     City: { type: String },
     UserName: { type: String },
     Password: { type: String },
