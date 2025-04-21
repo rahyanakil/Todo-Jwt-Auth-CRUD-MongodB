77ecb9918776337782bc3d87c8635ba38dc51361
rahyanakil <rahyanakil89@gmail.com>
2025-04-21 16:59:50 +0600

    updated login and select profile

--- src/controllers/ProfileController.js
+++ src/controllers/ProfileController.js
@@ -1,5 +1,5 @@
 const ProfileModel = require("../models/ProfileModel");
-
+var jwt = require('jsonwebtoken');
 exports.CreateProfile = (req, res) => {
   let reqBody = req.body;
   ProfileModel.create(reqBody, (err, data) => {
@@ -9,4 +9,54 @@ exports.CreateProfile = (req, res) => {
       res.status(200).json({ status: "success", data: data });
     }
   });
+};
+
+//user login
+exports.UserLogin = (req, res) => {
+  let reqBody  = req.body;
+  let UserName =reqBody['UserName']
+  let Password =reqBody['Password']
+  ProfileModel.find({UserName:UserName,Password:Password},(err,data)=>{
+    if(err){
+      res.status(400).json({ status: "fail", data: err });
+    }
+    else{
+      if(data.length>0){
+
+        //creating json  auth token
+        let payload ={
+          exp: Math.floor(Date.now() / 1000) + (24* 60 * 60),
+          data:data[0]
+        }
+        var token = jwt.sign(payload,'SecretKey123456789');
+
+        res.status(200).json({status:"Successfully Login",token:token,data:data})
+        
+      }
+      else{
+        res.status(401).json({status:"Unauthorized"})
+      }
+    }
+    
+  })
+  
+};
+
+
+
+
+exports.SelectProfile = (req, res) => {
+  let UserName =req.headers['username']
+
+  ProfileModel.find({UserName:UserName},(err,data)=>{
+    
+    if (err) {
+      res.status(400).json({ status: "fail", data: err });
+    } else {
+      res.status(200).json({ status: "success", data: data });
+    }
+    
+    
+  })
+  
 };
--- src/routes/api.js
+++ src/routes/api.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const ProfileController=require("../controllers/ProfileController")
+const AuthVerifyMiddleware=require("../middleware/AuthVerifyMiddleware")
 const router = express.Router();
 
 
 
 router.post('/CreateProfile',ProfileController.CreateProfile)
+router.post('/UserLogin',ProfileController.UserLogin)
+router.get("/SelectProfile",AuthVerifyMiddleware,ProfileController.SelectProfile)
 
 
 module.exports =router;
